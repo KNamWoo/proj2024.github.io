8f7b9896592e5aff7b78083c3567daf13869cb2e
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-04-03 11:34:26 +0900

    update

--- index.js
+++
@@ -1,3 +0,0 @@
-function go_login(){
-    location.href="https://knamwoo.github.io/login.html";
-}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+function go_login(){
+    location.href="https://knamwoo.github.io/proj2024.github.io/html/login.html";
+}
