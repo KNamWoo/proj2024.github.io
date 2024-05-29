b744c874bb53b13db54aa821f2f833829a06e0b7
김남우 <121301972+KNamWoo@users.noreply.github.com>
2024-05-29 11:11:55 +0900

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-function go_search(){
-    location.href="https://knamwoo.github.io/proj2024.github.io/html/search.html"
-}
-
 function go_map(){
     location.href="https://knamwoo.github.io/proj2024.github.io/html/navi.html"
 }
--- js/search.js
+++
@@ -1,1 +0,0 @@
-location.href="../html/navi.html"+address;
